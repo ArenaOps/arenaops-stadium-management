04ae9609352ab15ab47c509c03456bd0814f3787
mjasil <nafilatk7@gmail.com>
2026-02-16 15:57:09 +0530

    ARENA-13-Build Signup Page

--- FRONTEND/arenaops-web/src/app/(auth)/register/page.tsx
+++ FRONTEND/arenaops-web/src/app/(auth)/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/auth/Registerform";
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#dfe7f5] to-[#c3d0ec]">
+    <div className="min-h-screen flex items-center justify-center ">
       <RegisterForm />
     </div>
   );
