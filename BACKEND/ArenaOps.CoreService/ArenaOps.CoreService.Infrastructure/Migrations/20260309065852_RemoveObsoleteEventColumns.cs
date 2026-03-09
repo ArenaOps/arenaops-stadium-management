6bbfefa2427dd2d999d6f347446b725ac691032c
Rashi003mp <muhammedrashidr222@gmail.com>
2026-03-09 12:36:13 +0530

    Fix Event Creation: Remove obsolete StartTime column

---
+++ BACKEND/ArenaOps.CoreService/ArenaOps.CoreService.Infrastructure/Migrations/20260309065852_RemoveObsoleteEventColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArenaOps.CoreService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveObsoleteEventColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
